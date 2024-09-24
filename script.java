fba01970a286c1dc61e5ceada2ec0060970a5342
Jar0705 <156736264+Jar0705@users.noreply.github.com>
2024-09-24 22:13:55 +0700

    Update script.java

--- script.java
+++ script.java
@@ -32,8 +32,8 @@ class Mahasiswa {
 public class Main {
     public static void main(String[] args) {
         // Membuat objek Mahasiswa
-        Mahasiswa mahasiswa1 = new Mahasiswa("Fajar Nr", "123456", "Informatika");
-        Mahasiswa mahasiswa2 = new Mahasiswa("Fajar Nr", "654321", "Sistem Informasi");
+        Mahasiswa mahasiswa1 = new Mahasiswa("Fajar Dwi", "123456789", "Informatika");
+        Mahasiswa mahasiswa2 = new Mahasiswa("Fajar Dwi", "6513t3 "Sistem Informasi");
 
         // Menggunakan metode objek
         mahasiswa1.belajar();  // Output: Budi sedang belajar.
